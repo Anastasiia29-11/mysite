05debe9cb741a50b554d11dbed642f5358ee3232
Anastasiia29-11 <56249058+Anastasiia29-11@users.noreply.github.com>
2019-12-11 01:38:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include('auth.php');
 <meta charset="utf-8"> 
 <link rel="stylesheet" href="style.css">
 </head> 
-<body class="body"> 
+<body class="ip"> 
 <div class="caf"> <p align="center"> Кафе "У Палны" </p></div>
 <div class="form"> 
 	<p align="center"><?php echo $_SESSION['login']; ?> </br> 
@@ -18,7 +18,7 @@ include('auth.php');
 
 <table border="1px" bordercolor="white" width="300" height="50" bgcolor="#666666">
 <tr>
-<td align="center"><a class="col1" href="menu.php"> Меню </a></td>
+<td align="center"><a class="col" href="menu.php"> Меню </a></td>
 <td align="center"><a class="col" href="akci.php"> Акции </a></td>
 <td align="center"><a class="col" href="kontakt.php"> Контакты </a></td>
 </tr>
@@ -28,7 +28,9 @@ include('auth.php');
 <img src="img_58.jpg" alt="Картинка">
 </div>
 <div class="top">
-<p> gcghcycjcxhg,hv</p>
+<p class="il" align="center"> Принципы </p><p align="center"> #1: НАТУРАЛЬНОЕ </br></br> Мы используем только натуральные продукты. Никаких заменителей, никаких компромиссов!</br></br></br>
+#2: СВЕЖЕЕ </br></br> Мы не экономим на свежести и качестве: каждый день отбираем свежие продукты и готовим с вдохновением и большой ответственностью на нашей большой кухне.</br>
+ </p>
 </div></div></br>
 
 </body> 
