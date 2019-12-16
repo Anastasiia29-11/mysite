e178d8cf8dcfc2c80099e34b38cdfecfb189059e
Anastasiia29-11 <56249058+Anastasiia29-11@users.noreply.github.com>
2019-12-16 10:37:20 +0300

    Update bronirovanie.php

--- bronirovanie.php
+++ bronirovanie.php
@@ -1,29 +1,17 @@
 <?php 
   $stolik = $_POST['stolik'];
-
- 
-
   $time = $_POST['time'];
-
- 
-    $date =  date("Y-m-d");
-
-  $email = $_POST['email'];
-
-  $login = $_POST['login'];
-
+$date =  $_POST['date'];
+$email = $_POST['email'];
+$login = $_POST['login'];
+$date= date("Y-m-d");
 
 $mysql =  mysqli_connect('localhost','root','','users');
-
- 
-
-  $mysql->query("INSERT INTO `bron` (`date`,`time`,`stolik`,`login`,`email`)
+$mysql->query("INSERT INTO `bron` (`date`,`time`,`stolik`,`login`,`email`)
 
   VALUES('$date','$time','$stolik','$login','$email')"); 
 
- 
-
-  $mysql->close();
+$mysql->close();
 
   header('Location:cong.php');
 
